a755a2f5e85ba30b3942b3ec537cda2f8f45f512
Greta-Alfredsdottir <eldvakinn@hotmail.com>
2026-06-04 10:54:01 +0000

    ...

--- src/data/cvData.js
+++ src/data/cvData.js
@@ -76,7 +76,7 @@ export const cvData = {
   experience: [
     {
       company: "Tækniskólinn",
-      role: "practic as an AI researcher",
+      role: "internship as an AI researcher",
       period: "maj-juni 2026",
       location: "Hafnarfjörður, Iceland",
       description: "Making an app using only AI",
